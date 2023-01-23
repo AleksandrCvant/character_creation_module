c4cae463248a9602c5ab7ea616878b568cc436ed
Александр Метелица <AlrksandrLenZap@yandex.ru>
2023-01-23 21:27:23 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def defence(char_name: str, char_class: str) -> str:
     """Расчет защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
-    if char_class == 'mage':
+    if char_class == 'mage': 
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
